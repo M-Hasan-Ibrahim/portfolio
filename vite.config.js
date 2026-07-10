249d538178cd012f62575d20241641fcce41c0c4
M-Hasan-Ibrahim <mohamadhasan0711@gmail.com>
2026-07-10 12:45:47 +0300

    fixing hosting bugs in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] || "portfolio";
-
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? `/${repoName}/` : "/",
+  base: "/",
   plugins: [react()],
 });
